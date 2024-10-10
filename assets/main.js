208a9b93a5a4a07e67dfdd4f7975e8a485b9495d
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-10 18:31:40 +0200

    refactor: delete unnecessary code from js, unnecessary attributes from html

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,3 @@
-console.log('im alive');
-
-// Descrizione:
-// Visualizzare in pagina 5 numeri casuali.
-// Da lì parte un timer di 30 secondi.
-// Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-// NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
 // BONUS:
 // Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
 // Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.
@@ -66,19 +58,9 @@ function timer() {
 }
 
 // 4. lettura valori del form e salvataggio nel nuovo array
-// evento test
 checkButton.addEventListener('click', function (e) {
     e.preventDefault();
-
-    // lettura valori del form
-    const inputOne = Number(formOne.value);
-    const inputTwo = Number(formTwo.value);
-    const inputThree = Number(formThree.value);
-    const inputFour = Number(formFour.value);
-    const inputFive = Number(formFive.value);
-
     arrCheck();
-    console.log(inputOne, inputTwo, inputThree, inputFour, inputFive);
 })
 
 console.log(`arrSimon: ${arrSimon}, userCheck: ${userCheck}`);
@@ -103,5 +85,3 @@ function arrCheck() {
     countOutput.innerHTML = arrCount;
     numCheckOutput.innerHTML = numFound;
 }
-
-// perchè il check non funziona a dovere?
